858d2a5796a2c31738fb8c1597238786931ed4e4
pateltithi010 <pateltithi010@gmail.com>
2025-12-08 14:19:12 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="container mt-4 text-center">
+      <h1>Welcome to My App</h1>
+      <p>This is a component.</p>
+    </div>
+  )
+}
+
+export default Home;
---
+++ Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-dark p-3">
+      <h2 className="text-white">My App</h2>
+    </nav>
+  )
+}
+
+export default Navbar;
